d2c7dfb415537714331d432df7432ce639230844
Gauthier Jolly <contact@gjolly.fr>
2025-06-15 17:24:57 +0200

    tests: don't install dependencies in tests

--- tests/download-tests.sh
+++ tests/download-tests.sh
@@ -4,14 +4,6 @@ PACKAGE="btop"
 IMAGE="ubuntu-24.04-server-cloudimg-amd64"
 IMAGE_URL="https://cloud-images.ubuntu.com/releases/noble/release/$IMAGE.img"
 
-source "/etc/os-release"
-
-if [ "$ID" == "ubuntu" ]; then
-    echo "Installing dependencies"
-    apt-get update
-    apt-get install -y qemu-system-x86 qemu-utils
-fi
-
 echo "Running integration tests with image $IMAGE_URL"
 ./target/debug/proposed_package_testing "$IMAGE_URL" "$PACKAGE"
 
--- tests/qcow2-test.sh
+++ tests/qcow2-test.sh
@@ -4,14 +4,6 @@ PACKAGE="btop"
 IMAGE="ubuntu-24.04-server-cloudimg-amd64"
 IMAGE_URL="https://cloud-images.ubuntu.com/releases/noble/release/$IMAGE.img"
 
-source "/etc/os-release"
-
-if [ "$ID" == "ubuntu" ]; then
-    echo "Installing dependencies"
-    apt-get update
-    apt-get install -y qemu-system-x86 qemu-utils
-fi
-
 if [ ! -f "$IMAGE.img" ]; then
     echo "Downloading image $IMAGE.img from $IMAGE_URL"
     curl -LO "$IMAGE_URL"
